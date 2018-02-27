7f91e6d07bf565006328e452ceba73d0113d99a4
Martin Bergeron <bergerm2@hotmail.com>
2018-02-27 16:00:40 -0500

    Add MethodeCombat

---
+++ Game.Library/MethodeCombat.cs
@@ -0,0 +1,94 @@
+﻿using Game.Library.Enums;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Library
+{
+    public class MethodeCombat
+    {
+        public static double Dommage(Elements elementattaque, Elements elementdefense)
+        {
+            switch (elementattaque)
+            {
+                case Elements.Physique:
+                    if (elementdefense == Elements.Etheral)
+                    {
+                        return 0.5;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                case Elements.Etheral:
+
+                    return 1;
+
+                case Elements.Air:
+
+                    if (elementdefense == Elements.Eau)
+                    {
+                        return 1.5;
+                    }
+                    else if (elementdefense == Elements.Terre)
+                    {
+                        return 0.5;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+
+                case Elements.Feu:
+                    if (elementdefense == Elements.Terre)
+                    {
+                        return 1.5;
+                    }
+                    else if (elementdefense == Elements.Eau)
+                    {
+                        return 0.5;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                case Elements.Eau:
+                    if (elementdefense == Elements.Feu)
+                    {
+                        return 1.5;
+                    }
+                    else if (elementdefense == Elements.Air)
+                    {
+                        return 0.5;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                 case Elements.Terre:
+                    if (elementdefense == Elements.Feu)
+                    {
+                        return 0.5;
+                    }
+                    else if (elementdefense == Elements.Air)
+                    {
+                        return 1.5;
+                    }
+                    else
+                    {
+                        return 1;
+                    }   
+                    
+                 default:
+                    break;
+            }
+        }
+        
+
+
+    }
+}
