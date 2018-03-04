553854838c16793f43533d69c6099543cf0a2b14
Alexandre Caron <alexcaron880@gmail.com>
2018-03-04 14:49:10 -0500

    Afficgae mng

---
+++ TestConsoleJeu/AffichageManager/AffichageManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Lidgren.Network;
+
+namespace TestConsoleJeu.AffichageManager
+{
+    public class AffichageManager
+    {
+        public NetClient Client;
+
+        public void Init()
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("FinalProjet");
+            Client = new NetClient(config);
+            Client.Start();
+            Client.Connect("localhost", 14242);
+        }
+
+        public void SendLoop()
+        {
+            NetIncomingMessage message;
+            var stop = false;
+
+            while (!stop)
+            {
+                var mOut = Client.CreateMessage();
+                mOut.ReadAllFields(JeuProjetTest.Player);
+                Client.SendMessage(mOut, Client.ServerConnection, NetDeliveryMethod.ReliableOrdered);
+
+
+                while ((message = Client.ReadMessage()) != null)
+                {
+                    switch (message.MessageType)
+                    {
+                        case NetIncomingMessageType.Data:
+                            {
+
+                            }
+
+                            break;
+                        case NetIncomingMessageType.DebugMessage:
+                            Console.WriteLine(message.ReadString());
+                            break;
+                        case NetIncomingMessageType.StatusChanged:
+                            Console.WriteLine(message.SenderConnection.Status);
+                            if (message.SenderConnection.Status == NetConnectionStatus.Connected)
+                            {
+
+                                Console.WriteLine("{0} has connected.",
+                                    message.SenderConnection.Peer.Configuration.LocalAddress);
+                            }
+
+                            if (message.SenderConnection.Status == NetConnectionStatus.Disconnected)
+                            {
+                                Console.WriteLine("{0} has disconnected.",
+                                    message.SenderConnection.Peer.Configuration.LocalAddress);
+                            }
+
+                            break;
+                        case NetIncomingMessageType.WarningMessage:
+                            Console.WriteLine("Warning Message type: " + message.ReadString());
+
+                            break;
+                        default:
+                            Console.WriteLine($"Unhandled message type: {message.MessageType}");
+                            break;
+                    }
+
+                    Client.Recycle(message);
+                }
+            }
+        }
+
+    }
+}
